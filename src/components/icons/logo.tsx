00190e017f9415e485c0ccdd07acded7804ec917
spraiedutech <spraiedutech@gmail.com>
2025-09-16 18:00:15 +0000

    there is a error,, can you do something for app icon to animate upside d

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -9,18 +9,13 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       height="30"
       {...props}
     >
-      <defs>
-        <style>
-          {`
-            @import url('https://fonts.googleapis.com/css2?family=Literata:wght@700&display=swap');
-          `}
-        </style>
-      </defs>
-      <g className="fill-primary" fill="hsl(var(--primary))">
-        <path d="M20 10 Q 22 15, 20 22 L 20 40 L 18 40 L 18 22 Q 16 15, 18 10 Z" />
-        <path d="M14 12 Q 15 16, 14 20 L 14 25 L 12 25 L 12 20 Q 11 16, 12 12 Z" />
-        <path d="M26 12 Q 25 16, 26 20 L 26 25 L 28 25 L 28 20 Q 29 16, 28 12 Z" />
-        <path d="M14 12 A 6 6 0 0 1 26 12 L 24 12 A 4 4 0 0 0 16 12 Z" />
+      <g className="fill-primary animate-flip" fill="hsl(var(--primary))" style={{ transformOrigin: '20px 25px' }}>
+        <g transform="rotate(180, 20, 25)">
+          <path d="M20 10 Q 22 15, 20 22 L 20 40 L 18 40 L 18 22 Q 16 15, 18 10 Z" />
+          <path d="M14 12 Q 15 16, 14 20 L 14 25 L 12 25 L 12 20 Q 11 16, 12 12 Z" />
+          <path d="M26 12 Q 25 16, 26 20 L 26 25 L 28 25 L 28 20 Q 29 16, 28 12 Z" />
+          <path d="M14 12 A 6 6 0 0 1 26 12 L 24 12 A 4 4 0 0 0 16 12 Z" />
+        </g>
       </g>
       <text
         x="40"
