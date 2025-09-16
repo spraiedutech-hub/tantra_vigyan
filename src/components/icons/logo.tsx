a8310280708396a2f0b6ff2c850ec786b632cbb3
spraiedutech <spraiedutech@gmail.com>
2025-09-16 17:53:01 +0000

    can you add trishul as a logo next to app name

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,8 +4,8 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 200 50"
-      width="120"
+      viewBox="0 0 240 50"
+      width="150"
       height="30"
       {...props}
     >
@@ -16,8 +16,14 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
           `}
         </style>
       </defs>
+      <g className="fill-primary" fill="hsl(var(--primary))">
+        <path d="M20 10 Q 22 15, 20 22 L 20 40 L 18 40 L 18 22 Q 16 15, 18 10 Z" />
+        <path d="M14 12 Q 15 16, 14 20 L 14 25 L 12 25 L 12 20 Q 11 16, 12 12 Z" />
+        <path d="M26 12 Q 25 16, 26 20 L 26 25 L 28 25 L 28 20 Q 29 16, 28 12 Z" />
+        <path d="M14 12 A 6 6 0 0 1 26 12 L 24 12 A 4 4 0 0 0 16 12 Z" />
+      </g>
       <text
-        x="10"
+        x="40"
         y="35"
         fontFamily="'Literata', serif"
         fontSize="30"
